e447d67ecc7df9167de0acf3d9ce2e281fd0353e
Kyle Buchanan <kylebuch8@gmail.com>
2015-04-15 10:50:04 -0400

    added fast click

--- app/app.js
+++ app/app.js
@@ -11,4 +11,13 @@
     ])
 
         .constant('FIREBASE_URL', 'https://pollreal.firebaseio.com');
+
+    /*
+     * fastclick!!!
+     */
+     if ('addEventListener' in document) {
+         document.addEventListener('DOMContentLoaded', function() {
+             FastClick.attach(document.body);
+         }, false);
+     }
 }());
